932443903218f5159679e7d8b295f14a629c5188
Nox2903 <aryayudistira2903@gmail.com>
2024-07-11 12:35:13 +0700

    test dummy, pbr material :: pt 1

---
+++ IGDX Bootcamp/Assets/DynamicTextureTiling.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DynamicTextureTiling : MonoBehaviour
+{
+    // Reference to the original material with the texture
+    Material originalMaterial;
+
+    void Start()
+    {
+        // Ensure we have a material
+        originalMaterial = GetComponent<Renderer>().sharedMaterial;
+
+        // Create a new material instance for this object
+        Material materialInstance = new Material(originalMaterial);
+
+        // Apply the new material to the object
+        GetComponent<Renderer>().sharedMaterial = materialInstance;
+
+        // Get the initial scale of the object
+        Vector3 initialScale = transform.localScale;
+
+        // Set the texture tiling based on the initial scale
+        SetTextureTiling(materialInstance, initialScale);
+    }
+
+    void Update()
+    {
+        // Adjust texture tiling based on the current scale
+        SetTextureTiling(GetComponent<Renderer>().sharedMaterial, transform.localScale);
+    }
+
+    void SetTextureTiling(Material material, Vector3 scale)
+    {
+        // Calculate tiling based on the scale
+        Vector2 tiling = new Vector2(scale.x, scale.z);
+
+        // Apply tiling to the material
+        material.mainTextureScale = tiling;
+    }
+}
